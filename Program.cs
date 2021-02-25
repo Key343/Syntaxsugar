c3ccebb2a04ca6c4b56b23c6f2588dba99cf0fe5
Key343 <keyonna.sharde@gmail.com>
2021-02-24 21:17:35 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var answer = 4;
+            //var response = "";
+
+            var response = (answer < 9) ? "is less than nine " : "greater tam equal to nine";
+            Console.WriteLine($" {answer} {response}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
